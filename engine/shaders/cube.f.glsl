25e70afc4b13a5670abd488e39a163f716ad8467
Luke Donovan <ladbuilder1@gmail.com>
2021-04-20 14:54:27 -0700

    slim down

--- engine/shaders/cube.f.glsl
+++
@@ -1,8 +0,0 @@
-varying vec3 f_color;
-varying vec2 f_coord_uv;
-uniform sampler2D texture;
-
-void main(void) {
-    vec2 flipped_coord_uv = vec2(f_coord_uv.x, 1.0 - f_coord_uv.y);
-    gl_FragColor = texture2D(texture, flipped_coord_uv);
-}
